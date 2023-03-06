22848bd8640e478a23e5bd40e4f3902fa2111016
Gabriele Sarti <gabriele.sarti996@gmail.com>
2023-03-06 23:37:20 +0100

    Migrate docs to `furo` (#168
    * Migrate docs to furo
    
    * fix Chrome style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, str(_PATH_ROOT.absolute()))
 # -- Project information -----------------------------------------------------
 
 project = "inseq"
-copyright = "2021, The Inseq Team, Licensed under the Apache License, Version 2.0"
+copyright = "2023, The Inseq Team, Licensed under the Apache License, Version 2.0"
 author = "The Inseq Team"
 
 # The short X.Y version
@@ -114,7 +114,11 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+
+html_logo = "images/inseq_logo.png"
+
+html_title = f"inseq {release}"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -122,13 +126,42 @@ html_theme = "sphinx_rtd_theme"
 #
 html_theme_options = {
     "navigation_with_keys": True,
-    "logo_only": True,
-    "collapse_navigation": False,
-    "navigation_depth": 4,
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/inseq-team/inseq",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/InseqLib",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em">
+                    <path d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0 0 75-94 336.64 336.64 0 0 1-108.2 41.2A170.1 170.1 0 0 0 672 174c-94.5 0-170.5 76.6-170.5 170.6 0 13.2 1.6 26.4 4.2 39.1-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 0 0-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 0 1-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 0 1-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850 671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2 33.2-24.3 62.3-54.4 85.5-88.2z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+        {
+            "name": "InDeep Consortium",
+            "url": "https://interpretingdl.github.io",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M333.78 20.188c-39.97 0-79.96 15.212-110.405 45.656-58.667 58.667-60.796 152.72-6.406 213.97l-15.782 15.748 13.25 13.25 15.75-15.78c61.248 54.39 155.3 52.26 213.968-6.407 60.887-60.886 60.888-159.894 0-220.78C413.713 35.4 373.753 20.187 333.78 20.187zm0 18.562c35.15 0 70.285 13.44 97.158 40.313 53.745 53.745 53.744 140.6 0 194.343-51.526 51.526-133.46 53.643-187.5 6.375l.218-.217c-2.35-2.05-4.668-4.17-6.906-6.407-2.207-2.206-4.288-4.496-6.313-6.812l-.218.22c-47.27-54.04-45.152-135.976 6.374-187.502C263.467 52.19 298.63 38.75 333.78 38.75zm0 18.813c-30.31 0-60.63 11.6-83.81 34.78-46.362 46.362-46.362 121.234 0 167.594 10.14 10.142 21.632 18.077 33.905 23.782-24.91-19.087-40.97-49.133-40.97-82.94 0-15.323 3.292-29.888 9.22-43-4.165 20.485.44 40.88 14.47 54.907 24.583 24.585 68.744 20.318 98.624-9.562 29.88-29.88 34.146-74.04 9.56-98.625-2.375-2.376-4.943-4.473-7.655-6.313 45.13 8.648 79.954 46.345 84.25 92.876 4.44-35.07-6.82-71.726-33.813-98.72-23.18-23.18-53.47-34.78-83.78-34.78zM176.907 297.688L42.094 432.5l34.562 34.563L211.47 332.25l-34.564-34.563zM40 456.813L24 472.78 37.22 486l15.968-16L40 456.812z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
 }
 
 ogp_image = ""
-ogp_description = "InSeq: Interpretability for Sequence Generation Models"
+ogp_description = "Inseq: an Interpretability Toolkit for Sequence Generation Models"
 ogp_description_length = 55
 
 ogp_custom_meta_tags = [
@@ -143,14 +176,9 @@ html_static_path = [
     "images",
     "html_outputs",
     "_static",
-    "_static/style.css",
-    "_static/hk-grotesk-pro/HKGroteskPro-Bold.woff2",
-    "_static/hk-grotesk-pro/HKGroteskPro-Regular.woff2",
-    "_static/hk-grotesk-pro/HKGroteskPro-SemiBold.woff2",
-    "_static/hk-grotesk-pro/HKGroteskPro-Medium.woff2",
 ]
 
-html_css_files = ["style.css"]
+html_css_files = ["_static/inseq.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -276,5 +304,5 @@ sphinxemoji_style = "twemoji"
 
 
 def setup(app):
-    app.add_css_file("style.css")
     app.add_js_file("inseq.js")
+    app.add_css_file("inseq.css")
