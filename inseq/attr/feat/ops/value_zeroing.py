bd591396f1d7cdc00966c67e5734ee63a2bec8a9
Gabriele Sarti <gabriele.sarti996@gmail.com>
2023-04-20 11:28:13 +0200

    Added comments and docstrings to VZ

--- inseq/attr/feat/ops/value_zeroing.py
+++ inseq/attr/feat/ops/value_zeroing.py
@@ -32,11 +32,34 @@ logger = logging.getLogger(__name__)
 
 
 class ValueZeroing(Attribution, AggregableMixin):
-    SIMILARITY_METRICS = {
-        "cosine_distance": nn.CosineSimilarity(dim=-1),
-    }
+    """Value Zeroing method for feature attribution.
 
-    """Value Zeroing method for feature attribution."""
+    Introduced by `Mohebbi et al. (2023) <https://arxiv.org/abs/2301.12971>`__ to quantify context mixing inside
+    Transformer models. The method is based on the observation that context mixing is regulated by the value vectors
+    of the attention mechanism. The method consists of two steps:
+
+    1. Zeroing the value vectors of the attention mechanism for a given token index at a given layer of the model.
+    2. Computing the similarity between hidden states produced with and without the zeroing operation, and using it
+         as a measure of context mixing for the given token at the given layer.
+
+    The method is converted into a feature attribution method by allowing for extraction of value zeroing scores at
+    specific layers, or by aggregating them across layers.
+
+    Attributes:
+        SIMILARITY_METRICS (:obj:`Dict[str, Callable]`):
+            Dictionary of available similarity metrics to be used forvcomputing the distance between hidden states
+            produced with and without the zeroing operation. Converted to distances as 1 - produced values.
+        forward_func (:obj:`AttributionModel`):
+            The attribution model to be used for value zeroing.
+        clean_hidden_states (:obj:`Dict[int, torch.Tensor]`):
+            Dictionary to store the hidden states produced by the model without the zeroing operation.
+        corrupted_hidden_states (:obj:`Dict[int, torch.Tensor]`):
+            Dictionary to store the hidden states produced by the model with the zeroing operation.
+    """
+
+    SIMILARITY_METRICS = {
+        "cosine": nn.CosineSimilarity(dim=-1),
+    }
 
     def __init__(self, forward_func: "AttributionModel") -> None:
         super().__init__(forward_func)
@@ -45,6 +68,14 @@ class ValueZeroing(Attribution, AggregableMixin):
 
     @staticmethod
     def get_value_zeroing_hook(varname: str = "value") -> Callable[..., None]:
+        """Returns a hook to zero the value vectors of the attention mechanism.
+
+        Args:
+            varname (:obj:`str`, optional): The name of the variable containing the value vectors. The variable
+                is expected to be a 3D tensor of shape (batch_size, num_heads, seq_len) and is retrieved from the
+                local variables of the execution frame during the forward pass.
+        """
+
         def value_zeroing_forward_mid_hook(frame, value_zeroing_index: Optional[int] = None) -> None:
             # Zeroing value vectors corresponding to the given token index
             if value_zeroing_index is not None:
@@ -54,6 +85,14 @@ class ValueZeroing(Attribution, AggregableMixin):
         return value_zeroing_forward_mid_hook
 
     def get_states_extract_and_patch_hook(self, layer: int, hidden_state_index: int = 0) -> Callable[..., None]:
+        """Returns a hook to extract the produced hidden states (corrupted by value zeroing)
+          and patch them with pre-computed clean states that will be passed onwards in the model forward.
+
+        Args:
+            layer (:obj:`int`): The layer at which the hook is applied. Used to store the extracted states.
+            hidden_state_index (:obj:`int`, optional): The index of the hidden state in the model output tuple.
+        """
+
         def states_extract_and_patch_forward_hook(module, args, output) -> None:
             device = output[hidden_state_index].device
             self.corrupted_hidden_states[layer] = output[hidden_state_index].clone().detach().cpu()
@@ -77,7 +116,7 @@ class ValueZeroing(Attribution, AggregableMixin):
         additional_forward_args: TensorOrTupleOfTensorsGeneric,
         layers: Union[int, Tuple[int, int], List[int], None] = None,
         aggregate_layers_fn: Union[str, AggregationFunction, None] = None,
-        metric: str = "cosine_distance",
+        similarity_metric: str = "cosine",
         encoder_hidden_states: Optional[MultiLayerEmbeddingsTensor] = None,
         decoder_hidden_states: Optional[MultiLayerEmbeddingsTensor] = None,
     ) -> TensorOrTupleOfTensorsGeneric:
@@ -89,38 +128,52 @@ class ValueZeroing(Attribution, AggregableMixin):
                 among the indices will be aggregated using aggregate_fn. If a list of indices is specified, the
                 respective layers will be used for aggregation. If aggregate_fn is "single", the last layer is
                 used by default. If no value is specified, all available layers are passed to aggregate_fn by default.
-            encoder_hidden_states (:obj:`torch.Tensor`, optional): A tensor of shape ``[num_layers+1, batch_size,
-                source_seq_len, hidden_size] containing hidden states of the encoder. Available only for
+            aggregate_layers_fn (:obj:`str` or :obj:`callable`): The method to use for aggregating across layers.
+                Can be one of `average` (default if layers is tuple or list), `max`, `min` or `single` (default if
+                layers is int or None), or a custom function defined by the user.
+            metric (:obj:`str`, optional): The similarity metric to use for computing the distance between hidden
+                states produced with and without the zeroing operation. Default: cosine.
+            encoder_hidden_states (:obj:`torch.Tensor`, optional): A tensor of shape ``[batch_size, num_layers,
+                source_seq_len, hidden_size]`` containing hidden states of the encoder. Available only for
                 encoder-decoders models. Default: None.
-            decoder_hidden_states (:obj:`torch.Tensor`, optional): A tensor of shape ``[num_layers, batch_size,
+            decoder_hidden_states (:obj:`torch.Tensor`, optional): A tensor of shape ``[batch_size, num_layers
                 target_seq_len, hidden_size]`` containing hidden states of the decoder.
 
         Returns:
             `TensorOrTupleOfTensorsGeneric`: Attribution outputs for source-only or source + target feature attribution
         """
+        if similarity_metric not in self.SIMILARITY_METRICS:
+            raise ValueError(
+                f"Similarity metric {similarity_metric} not available."
+                f"Available metrics: {','.join(self.SIMILARITY_METRICS.keys())}"
+            )
         batch_size = decoder_hidden_states.size(0)
         num_layers = self._num_layers(decoder_hidden_states)
         tgt_seq_len = decoder_hidden_states.size(2)
         decoder_stack: nn.ModuleList = find_block_stack(self.forward_func.get_decoder())
+        # Store clean hidden states for later use
         self.clean_hidden_states = {
             layer: decoder_hidden_states[:, layer, ...].clone().detach().cpu() for layer in range(len(decoder_stack))
         }
         scores = torch.zeros(batch_size, num_layers, tgt_seq_len, tgt_seq_len)
 
         # Hooks:
-        #   1. forward_hook on transformer block is used to hijack "hidden_states" (config var depending on
-        #      model) and force original hidden states ("decoder_hidden_states" variable). It also records the
-        #      output of the block (after value-zeroing) in a dictionary. This output would normally be passed
-        #      to the next layer, but gets swapped out by the step above.
-        #   2. pre_forward_hook to perform the value zeroing by dynamically replacing the intermediate "value"
-        #      tensor in the forward (name is config-dependent) with a zeroed version.
+        #   1. states_extract_and_patch_hook on the transformer block stores corrupted states and force clean states
+        #      as the output of the block forward pass, i.e. the zeroing is done independently across layers.
+        #   2. value_zeroing_block_hook on the attention module performs the value zeroing by dynamically replacing
+        #      the intermediate "value" tensor in the forward (name is config-dependent) with a zeroed version for the
+        #      specified token index.
+        #
+        # State extraction hooks can be registered only once since they are token-independent
         states_extraction_hook_handles = []
         for block_idx, block in enumerate(decoder_stack):
             states_extract_and_patch_hook = self.get_states_extract_and_patch_hook(block_idx, hidden_state_index=0)
             states_extraction_hook_handles.append(block.register_forward_hook(states_extract_and_patch_hook))
 
+        # Zeroing is done for every token in the target sequence separately (O(n) complexity)
         for token_idx in range(tgt_seq_len):
             value_zeroing_hook_handles = []
+            # Value zeroing hooks are registered for every token separately since they are token-dependent
             for block in decoder_stack:
                 attention_module = block.get_submodule(self.forward_func.config.attention_module)
                 value_zeroing_block_hook = get_post_variable_assignment_hook(
@@ -131,12 +184,14 @@ class ValueZeroing(Attribution, AggregableMixin):
                 )
                 value_zeroing_hook_handle = attention_module.register_forward_pre_hook(value_zeroing_block_hook)
                 value_zeroing_hook_handles.append(value_zeroing_hook_handle)
+            # Run forward pass with hooks. Fills self.corrupted_hidden_states with corrupted states across layers
+            # when zeroing the specified token index.
             with torch.no_grad():
                 self.forward_func(*inputs, *additional_forward_args)
             for handle in value_zeroing_hook_handles:
                 handle.remove()
             for block_idx in range(len(decoder_stack)):
-                similarity_scores = self.SIMILARITY_METRICS[metric](
+                similarity_scores = self.SIMILARITY_METRICS[similarity_metric](
                     self.clean_hidden_states[block_idx], self.corrupted_hidden_states[block_idx]
                 )
                 scores[:, block_idx, :, token_idx] = torch.ones_like(similarity_scores) - similarity_scores
@@ -147,14 +202,15 @@ class ValueZeroing(Attribution, AggregableMixin):
         per_token_sum_score = scores.sum(dim=-1, keepdim=True)
         per_token_sum_score[per_token_sum_score == 0] = 1
         scores = scores / per_token_sum_score
-        # [batch_size, tgt_seq_len, tgt_seq_len]
+        # Aggregate scores across layers to obtain final attribution scores
+        # Aggregation output has shape: [batch_size, tgt_seq_len, tgt_seq_len]
         scores = self._aggregate_layers(scores, aggregate_layers_fn, layers)
         # Since presently Inseq attribution is constrained by the attribution loop over the full generation, the
         # extraction of value zeroing scores is done inefficiently by picking only the last token scores at every step.
         # This makes the complexity of calling this method O(n^2), when it could be O(n) if the scores were extracted
         # only at the final step. Since other methods (e.g. attention) face the same issue, this will be addressed in
         # future releases.
-        return scores[:, :, -1]
+        return scores[:, -1, :]
         # TODO: Add support for encoder-decoder models
         # if is_encoder_decoder:
         #    encoder_hidden_states = torch.stack(outputs.encoder_hidden_states)
