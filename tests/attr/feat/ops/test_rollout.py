a28911b9503816755d879239743157e7744cc58c
Gabriele Sarti <gabriele.sarti996@gmail.com>
2023-05-01 16:47:44 +0200

    Fixes to dec-only rollout (still problematic)

--- tests/attr/feat/ops/test_rollout.py
+++ tests/attr/feat/ops/test_rollout.py
@@ -4,7 +4,7 @@ from inseq.attr.feat.ops.rollout import rollout_fn
 
 
 def test_rollout_consistency_custom_dim():
-    original = torch.randint(low=0, high=10, size=(1, 2, 3, 4, 5, 6, 2, 2))
+    original = torch.randn(1, 2, 3, 4, 5, 6, 2, 2)
     rolled_out_original = rollout_fn(original)
     dim_changed_2 = original.permute(0, 2, 1, 3, 4, 5, 6, 7)
     rolled_out_dim_changed_2 = rollout_fn(dim_changed_2, dim=2)
