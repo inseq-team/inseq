ab97de153a7e9ad23527b3a427eb49f741c3ec6c
Gabriele Sarti <gabriele.sarti996@gmail.com>
2023-12-07 09:31:38 +0800

    Update readme and version info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = "The Inseq Team"
 # The short X.Y version
 version = "0.5"
 # The full version, including alpha/beta/rc tags
-release = "0.5.0.dev0"
+release = "0.5.0"
 
 
 # Prefix link to point to master, comment this during version release and uncomment below line
