86a704d0f6b3948c9e86bcb9f0125618a6160320
Gabriele Sarti <gabriele.sarti996@gmail.com>
2024-01-09 15:16:17 +0100

    Fix docstring decorator

--- inseq/commands/commands_utils.py
+++ inseq/commands/commands_utils.py
@@ -8,13 +8,13 @@ def command_args_docstring(cls):
     A decorator that automatically generates a Google-style docstring for a dataclass.
     """
     docstring = f"{cls.__name__}\n\n"
-    field_names = dataclasses.fields(cls)
+    fields = dataclasses.fields(cls)
     resolved_hints = typing.get_type_hints(cls)
-    resolved_field_types = {name: resolved_hints[name] for name in field_names}
-    if field_names:
+    resolved_field_types = {field.name: resolved_hints[field.name] for field in fields}
+    if fields:
         docstring += "**Attributes:**\n"
-        for field in field_names:
-            field_type = resolved_field_types[field]
+        for field in fields:
+            field_type = resolved_field_types[field.name]
             field_help = field.metadata.get("help", "")
             docstring += textwrap.dedent(
                 f"""
