56ed5c4ec15e6da71b2e6eaf592567676f155e6f
Luuk Suurmeijer <luuksuurmeijer@gmail.com>
2024-07-18 13:24:55 +0200

    fixed json decode error

--- inseq/utils/torch_utils.py
+++ inseq/utils/torch_utils.py
@@ -85,7 +85,7 @@ def dequantize_safetensor(safetensor: bytes) -> torch.Tensor:
     Adapted from https://huggingface.co/docs/safetensors/metadata_parsing
     """
     header_length = struct.unpack("<Q", safetensor[:8])[0]
-    metadata = json.loads(safetensor[8 : (7 + header_length)])["__metadata__"]
+    metadata = json.loads(safetensor[8 : (8 + header_length)])["__metadata__"]
     recovered_tensor = safetensors.torch.load(safetensor)["attribution"].to(torch.float32)
     if metadata["quantization"] in ["float32", "float16"]:
         return recovered_tensor
